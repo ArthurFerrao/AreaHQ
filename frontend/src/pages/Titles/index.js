e2f138c9522db746bf4ee17a70390ad8ef752f4d
Arthur Ferrão <arthurdeferrao@gmail.com>
2020-10-29 22:47:51 -0300

    Add axios and get list of titles from API

--- frontend/src/pages/Titles/index.js
+++ frontend/src/pages/Titles/index.js
@@ -1,29 +1,18 @@
-import React, { useState } from 'react'
-import TitleCard from '../../components/TitleCard'
+import React, { useState, useEffect } from 'react';
+import TitleCard from '../../components/TitleCard';
+import API from '../../utils/API.js';
 
 import './styles.css';
 
 const Titles = () => {
-  const [titles, setTitles] = useState([
-    {
-      name: "Hero Academy",
-      type: "Quadrinho",
-      categories: [
-        "Ação", "Aventura"
-      ],
-      image: "https://ptanime.com/wp-content/uploads/2019/01/Boku-no-Hero-Academia-Volume-22_-5-de-fevereiro-2019_confirmation-pending.jpg",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
-    },
-    {
-      name: "Hero Academy",
-      type: "Quadrinho",
-      categories: [
-        "Ação", "Aventura"
-      ],
-      image: "https://ptanime.com/wp-content/uploads/2019/01/Boku-no-Hero-Academia-Volume-22_-5-de-fevereiro-2019_confirmation-pending.jpg",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
-    }
-  ]) 
+  const [titles, setTitles] = useState([]);
+
+  useEffect(() => {
+    API.get("titles")
+      .then(res => {
+        setTitles(res.data);
+      });
+  }, []);
 
   return ( 
     <div className="titles">
