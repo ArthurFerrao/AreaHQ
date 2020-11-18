8e98dd1b17ca5e4a115cf785d65c15028767ea2a
Arthur Ferrão <arthurdeferrao@gmail.com>
2020-11-18 10:56:21 -0300

    Add dropdown menu to navbar

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -1,23 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchBar from '../SearchBar'
+import MenuIcon from '@material-ui/icons/Menu';
 import { Link } from 'react-router-dom';
 
 import './styles.css';
 
 const NavBar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <nav className='nav-bar'>
-      <Link className='logo' to='/'>Área<span>HQ</span></Link>
-      <SearchBar/>
+      <div className='nav-bar-container'>
+        <Link className='logo' to='/'>Área<span>HQ</span></Link>
+        <SearchBar className='teste'/>
 
-      <div className='nav-btns'>
-        <Link className='nav-btn' to='/titles'>Títulos</Link>
-
-        <div className='nav-auth-btns'>
-          <Link className='nav-btn login-btn' to='/'>Login</Link>
-          <div className='sign-up-btn'>Quero publicar</div>
-        </div>
+        <MenuIcon id='hambuger-menu-icon' className='nav-btn' style={{ color: '#808080' }} fontSize='large' onClick={() => setOpenMenu(!openMenu)}/>
+        <ul className={openMenu? 'nav-btns-container menu-active': 'nav-btns-container'}>
+          <Link to='/titles' onClick={() => setOpenMenu(false)} className='nav-btn'>Títulos</Link>
+          <Link to='/' onClick={() => setOpenMenu(false)} className='nav-btn login-btn'>Login</Link>
+          <Link to='/' onClick={() => setOpenMenu(false)} className='sign-up-btn'>Quero publicar</Link>
+        </ul>
       </div>
+      
     </nav> 
   )
 }
